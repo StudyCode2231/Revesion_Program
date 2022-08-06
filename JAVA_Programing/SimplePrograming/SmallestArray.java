582b7d3f268013c00817c36b56f33bc0aa74b3aa
StudyCode2231 <1903044@kiit.ac.in>
2022-08-06 21:57:07 +0530

    :construction: file updated

---
+++ JAVA_Programing/SimplePrograming/SmallestArray.java
@@ -0,0 +1,22 @@
+package JAVA_Programing.SimplePrograming;
+import java.util.*;
+public class SmallestArray {
+    public static void main(String args[]){
+        int arr1[]={2,5,1,3,0};
+        System.out.println("The smallest value in the array"+Smallestelement(arr1));
+
+        int arr2[]={8,9,5,10,7};
+        System.out.println("The smallest value in the array"+Smallestelement(arr2));
+
+    }
+    static int Smallestelement(int arr[]){
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        return min;
+
+    }
+}
